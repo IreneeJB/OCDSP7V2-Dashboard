20507d07bfa7e1b837c74847c5a5912a373013a7
Irenee <ijosse@orange.fr>
2023-01-18 19:07:09 +0100

    test de sidebar

--- DashBoardStreamlit.py
+++ DashBoardStreamlit.py
@@ -23,24 +23,27 @@ prets = myclient.get_client_prets(id_value)
 importance = myclient.get_features_importance(id_value)
 
 # split client info and prets in tables
-client = pd.DataFrame(client, index = [1])
-client_perso_data = client.loc[:,['CODE_GENDER', 'DAYS_BIRTH', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS',
-                                    'CNT_CHILDREN', 'NAME_HOUSING_TYPE', 'REGION_POPULATION_RELATIVE',
-                                    'FLAG_MOBIL', 'FLAG_EMAIL', 'CNT_FAM_MEMBERS', ]]
-client_perso_data['DAYS_BIRTH'] = (0-client_perso_data['DAYS_BIRTH'])//365
-client_perso_data = client_perso_data.rename(columns={'DAYS_BIRTH': 'AGE'})
-client_poss_data = client.loc[:,['FLAG_OWN_CAR', 'OWN_CAR_AGE', 'FLAG_OWN_REALTY']]
-client_pro_data = client.loc[:,['AMT_INCOME_TOTAL', 'NAME_INCOME_TYPE', 'DAYS_EMPLOYED', 'FLAG_WORK_PHONE',
-                                'OCCUPATION_TYPE']]    
+try :
+    client = pd.DataFrame(client, index = [1])
+    client_perso_data = client.loc[:,['CODE_GENDER', 'DAYS_BIRTH', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS',
+                                        'CNT_CHILDREN', 'NAME_HOUSING_TYPE', 'REGION_POPULATION_RELATIVE',
+                                        'FLAG_MOBIL', 'FLAG_EMAIL', 'CNT_FAM_MEMBERS', ]]
+    client_perso_data['DAYS_BIRTH'] = (0-client_perso_data['DAYS_BIRTH'])//365
+    client_perso_data = client_perso_data.rename(columns={'DAYS_BIRTH': 'AGE'})
+    client_poss_data = client.loc[:,['FLAG_OWN_CAR', 'OWN_CAR_AGE', 'FLAG_OWN_REALTY']]
+    client_pro_data = client.loc[:,['AMT_INCOME_TOTAL', 'NAME_INCOME_TYPE', 'DAYS_EMPLOYED', 'FLAG_WORK_PHONE',
+                                    'OCCUPATION_TYPE']]
+except : 
+    pass
     
 if st.sidebar.button("Fiche client") :
     st.subheader(f"\n\t\tFiche Client n°{id_value}")
 
     # Bloc informations personnelles
     with st.expander("Informations personnelles") :
-        st.text(f"Age : {client_perso_data.loc['AGE']}")
-        st.text(f"Sexe : {client_perso_data.loc['CODE_GENDER']}")
-        st.text(f"Niveau d'éducation : {client_perso_data.loc['NAME_EDUCATION_TYPE']}")
+        st.text(f"Age : {client_perso_data.loc[:,'AGE'].iloc[0,0]}")
+        st.text(f"Sexe : {client_perso_data.loc[:,'CODE_GENDER']}")
+        st.text(f"Niveau d'éducation : {client_perso_data.loc[:,'NAME_EDUCATION_TYPE']}")
         st.text(f"Statut familial : {client_perso_data.loc[:,'NAME_FAMILY_STATUS']}")
         st.text(f"Nombre d'enfants : {client_perso_data.loc[:,'CNT_CHILDREN']}")
         st.text(f"Type d'habitation : {client_perso_data.loc[:,'NAME_HOUSING_TYPE']}")
